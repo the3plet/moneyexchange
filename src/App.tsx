1403124308139e0e5368555ec64ad6d2841de2f3
Srj <soorajp2002@gmail.com>
2025-01-14 01:38:52 +0530

    build fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Header from "./components/header";
 import Carousel from "./components/Carousel";
 import CardUI from "./components/CardUI";
 import Notification from "./components/Notification";
+import Nav from "./components/Nav";
 
 
 const App = () => {
   return (
     <div className="">
-      <Header />
+     <Nav/>
       <Carousel />
       <CardUI/>
       <Notification/>
---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+
+
+const Nav = () => {
+    return (
+      <div>
+          <img src="/logo.jpg" alt='logo' className='p-6 '></img>
+      </div>
+    )
+  }
+  
+  export default Nav;
